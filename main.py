bd4b8ab997836d72c3053e1d975f3c9a93da3e9a
Fractalskies <Skyhawkwarriors@gmail.com>
2026-08-21 19:54:38 +1000

    Added some stupid shit no one cares about

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-#TODO: GitHub repository.
+#TODO: Add textboxes. Turn into OOP instead of FOP.
 
 # #imports
 import pygame as pg
 import vars as v
+import render_txt as text
 
 #initialization
 pg.init()
 clock = pg.time.Clock()
 running = True
 dt = 0
+pg.display.set_caption("Pure happiness and joy")
+
 
 while running:
     for event in pg.event.get():
@@ -24,18 +27,22 @@ while running:
     #draws player sprite
     v.screen.blit(pg.transform.scale(v.vars_map["player_sprite"], (v.player_size[0], v.player_size[1])), (v.player_pos.x - (v.player_size[0]/2), v.player_pos.y - (v.player_size[1]/2)))
 
-    #Checks for movement
+    #draws the text box sprite
+    text.drawtxt()
+
+    #Checks for movement, fix it later
     keys = pg.key.get_pressed()
     if keys[pg.K_w] and v.map_pos[1] < -10:
         v.map_pos.y += 300 * dt
 
-    if keys[pg.K_s] and v.map_pos[1] > -v.map_size[0] + v.screen_size[1] + 10:
+    #Make this dynamic later i can't be fucked rn
+    if keys[pg.K_s] and v.map_pos[1] > -1600:
         v.map_pos.y -= 300 * dt
 
     if keys[pg.K_a] and v.map_pos[0] < -10:
         v.map_pos.x += 300 * dt
 
-    if keys[pg.K_d] and v.map_pos[0] > -v.map_size[1] + v.screen_size[0] + 10:
+    if keys[pg.K_d] and v.map_pos[0] > -v.map_size[1] + v.screen_size[1]:
         v.map_pos.x -= 300 * dt
 
     #flips the screen
--- vars.py
+++ vars.py
@@ -7,14 +7,26 @@ screen = pg.display.set_mode((screen_size[0], screen_size[1]))
 
 #setting player variables
 player_size = (100, 100)
-player_pos = pg.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_pos = pg.Vector2(screen_size[0] / 2, screen_size[1] / 2)
 
 #map data
-map_size = (1200, 1200)
+map_size = (screen_size[0]*4, screen_size[1]*4)
 map_pos = pg.Vector2(-200, -200)
+
+#textbox vars
+box_size = (900, 200)
+box_y = screen_size[1] - box_size[1]
+font_size = 32
+name_pos = ((screen_size[0]/4)*3, box_y + font_size/2)
+body_pos = (100, 200)
+
+#font
+pg.font.init()
+pfont = pg.font.Font("fonts/Pixel.ttf", font_size)
 
 #defining import hashmap
 vars_map = {}
+
 #defining import function
 def import_image():
     #Saves data from text file as list
